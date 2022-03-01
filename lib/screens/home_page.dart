39e59822e36560c2fd30b44e37737f4c75b72563
unknown <jhordanres@hotmail.com>
2022-03-01 12:05:01 -0500

    Home aplicación

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,43 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:[
-                Image.asset(
-                  'assets/images/logo.png',
-                  width: 150,
-                  height: 100,
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black, Colors.black87]
+            )
+          ),
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: DiagonalPathClipperTwo(),
+                child: Container(
+                  color: Colors.white,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children:[
+                      Image.asset(
+                        'assets/images/logo.png',
+                        width: 150,
+                        height: 100,
+                      ),
+                      const Text('Rapido y Seguro')
+                    ]
+                  ),
                 ),
-                const Text('Rapido y Seguro')
-              ]
-            ),
-            const SizedBox(height: 50),
-            const Text('SELECCIONA TU ROL'),
-            const SizedBox(height: 30),
-            const CircleAvatar(
-              backgroundImage: AssetImage('assets/images/pasajero.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            const SizedBox(height: 10),
-            const Text('Cliente'),
+              ),
+            
+              const SizedBox(height: 50),
+              const Text(
+                'SELECCIONA TU ROL',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+              const SizedBox(height: 30),
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/pasajero.png'),
+                radius: 50,
+                backgroundColor: Color.fromARGB(255, 78, 78, 78),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Cliente',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                ),
 
-            const SizedBox(height: 30),
-            const CircleAvatar(
-              backgroundImage: AssetImage('assets/images/driver.png'),
-              radius: 50,
-              backgroundColor: Colors.black,
-            ),
-            const SizedBox(height: 10),
-            const Text('Conductor'),  
-          ]
+              const SizedBox(height: 30),
+              const CircleAvatar(
+                backgroundImage: AssetImage('assets/images/driver.png'),
+                radius: 50,
+                backgroundColor: Color.fromARGB(255, 78, 78, 78),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Conductor',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                ),  
+            ]
+          ),
         ),
       ),
     );
